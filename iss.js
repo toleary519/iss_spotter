0174f8b6eccea0ee569669dbd7d4fc59ab2f26bb
vagrant <t.oleary@me.com>
2021-07-30 04:13:37 +0000

    linted and through fetching coords

---
+++ iss.js
@@ -0,0 +1,48 @@
+// iss.js
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(callback) {
+
+  request('https://freegeoip.app/json/', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `status code ${response.statusCode} when fetching coords: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const latitude = JSON.parse(body).latitude;
+    const longitude = JSON.parse(body).longitude;
+    const coords = {latitude, longitude};
+    callback(null, coords);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
