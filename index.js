027d168f788851ce3c487eb08200c0e9d326dc2d
vagrant <t.oleary@me.com>
2021-07-30 04:55:59 +0000

    final and linted

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // The code below is temporary and can be commented out.
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -13,11 +14,24 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-// fetchCoordsByIP((error, coords) => {
-//   if (error) {
-//     console.log("Coord Error", error);
-//     return;
-//   }
+fetchCoordsByIP((error, coords) => {
+  if (error) {
+    console.log("Coord Error", error);
+    return;
+  }
 
-//   console.log('It worked! Returned Coords:', coords);
-// });
+  console.log('It worked! Returned Coords:', coords);
+});
+
+
+
+const coords = { latitude: '48.4152', longitude: '-123.3655' };
+
+fetchFlyOverTimes(coords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
--- iss.js
+++ iss.js
@@ -40,9 +40,29 @@ const fetchCoordsByIP = function(callback) {
     const latitude = JSON.parse(body).latitude;
     const longitude = JSON.parse(body).longitude;
     const coords = {latitude, longitude};
+    // console.log(coords);
     callback(null, coords);
   });
 };
 
+const fetchFlyOverTimes = function(coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchFlyOverTimes };
